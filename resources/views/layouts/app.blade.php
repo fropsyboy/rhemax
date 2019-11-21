4489ee3a01d877dcd7d88d5533ea894481620ac1
fropsyboy <sabidacit@gmail.com>
2019-11-21 11:35:32 +0100

    RH-004

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Able Share SMS : SMS-Portal</title>
+    <title>RHEMAX : SMS-Portal</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
